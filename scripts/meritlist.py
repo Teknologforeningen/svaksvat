2bbb085abab967b9c270b389acc299f9b3f86b0a
Walliski <walliski@gmail.com>
2015-01-08 16:22:47 +0200

    Show type of membership for merit list

--- scripts/meritlist.py
+++ scripts/meritlist.py
@@ -29,21 +29,27 @@ def main():
     writer = csv.writer(open("meritlist.csv", "w"))
     members = session.query(Member).order_by(Member.surName_fld).all()
     for member in members:
+        for membership in member.membershipmemberships:
+            mship = membership.membership
+            currentmembership = ""
+            if membership.isCurrent():
+                currentmembership += mship.name_fld + " "
+
         if member.notes_fld:
-            writer.writerow([member.getWholeName(), "Hedersbetygelser:",
+            writer.writerow([currentmembership, member.getWholeName(), "Hedersbetygelser:",
                 member.notes_fld])
         for groupmembership in member.groupmemberships:
             if groupmembership.startTime_fld and groupmembership.endTime_fld:
                 group = groupmembership.group
                 for year in range(groupmembership.startTime_fld.date().year,
                         groupmembership.endTime_fld.date().year + 1):
-                    writer.writerow([member.getWholeName(), group.name_fld, year])
+                    writer.writerow([currentmembership, member.getWholeName(), group.name_fld, year])
         for postmembership in member.postmemberships:
             if postmembership.startTime_fld and postmembership.endTime_fld:
                 post = postmembership.post
                 for year in range(postmembership.startTime_fld.date().year,
                         postmembership.endTime_fld.date().year + 1):
-                    writer.writerow([member.getWholeName(), post.name_fld, year])
+                    writer.writerow([currentmembership, member.getWholeName(), post.name_fld, year])
     print("I am all done, kill me please.")
     sys.exit(0)
 
